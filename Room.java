8338024e59d1b45f701e0a26364e28aa9e9df1d4
saif-yousry <167723545+saif-yousry@users.noreply.github.com>
2024-05-03 23:30:25 +0300

    Update Room.java

--- Room.java
+++ Room.java
@@ -9,5 +9,36 @@ package safwa_project;
  * @author Cyber
  */
 public class Room {
-    
+    protected int room_num;
+    protected boolean available;
+    protected String room_type;
+
+    public Room(int room_num,String room_type) {
+        this.room_num = room_num;
+        this.available = true;
+        this.room_type = room_type;
+    }
+    public void book_room(){
+        if(available){
+            available=false;
+            System.out.println("The room num "+room_num+" is booked");
+        }else{
+            available=true;
+            System.out.println("The room "+room_num+" is not available");
+        }
+    }
+    public void check_in(){
+        if(available){
+            available=false;
+            System.out.println("Checked into "+room_num);
+        }else{
+            available=true;
+            System.out.println("The room "+room_num+" is not available");
+        }
+    }
+    public void check_out(){
+        available=true;
+        System.out.println("Checked out of "+room_num);
+    }
+
 }
